1a0b79ffa26641ed34218ed21aba2f24c83d8bda
lfgclub <degenOperator@proton.me>
2025-04-17 23:17:03 +0200

    Update depositContract.sol

--- src/depositContract.sol
+++ src/depositContract.sol
@@ -36,7 +36,7 @@
  */
 
 // SPDX-License-Identifier: LicenseRef-LFG-Commercial
-// Full licence: https://github.com/lfgclub/lfgclub/blob/main/LICENSE
+// Full license: https://github.com/lfgclub/lfgclub/blob/main/LICENSE
 
 import "./pool.sol";
 import "./factoryERC20.sol";
